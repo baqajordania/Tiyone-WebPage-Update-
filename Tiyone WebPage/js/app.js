f89462af33d0a4097682b54a45bd5b66797c2b45
baqajordania <bakari.jordania05b@geolab.edu.ge>
2017-06-09 17:17:39 +0400

    Add files via upload

---
+++ Tiyone WebPage/js/app.js
@@ -0,0 +1,26 @@
+var $ftsBtn = $('.ftt');
+
+var fts = function(){
+  alert('dsa');
+}
+
+
+$ftsBtn.click(fts);
+
+var fts = function(){
+ 	  $('.fortwodivs').css('display','flex');
+ 	  $('.fortwodivs1').css('display','none');
+ 	  $('.fortwodivs2').css('display','none');
+		}
+var sts = function(){
+ 	  $('.fortwodivs').css('display','none');
+ 	  $('.fortwodivs1').css('display','block');
+ 	  $('.fortwodivs2').css('display','none');
+		}
+
+var bts = function(){
+ 	  $('.fortwodivs').css('display','none');
+ 	  $('.fortwodivs1').css('display','none');
+ 	  $('.fortwodivs2').css('display','block');
+		}
+
